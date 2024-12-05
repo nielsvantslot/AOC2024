48ead43a221301c6a9ddde808860ebd9bfef1105
Niels van 't Slot <niels@kodea.nl>
2024-12-05 13:47:16 +0100

    filter out empty lines

--- template.js
+++ template.js
@@ -1,6 +1,8 @@
 export const run = (inputData) => {
   console.log("Received input data:", inputData);
-  const lines = inputData.split('\n');
+  const lines = inputData
+    .split('\n')
+    .filter(line => line.trim() !== '');
   console.log("Input file has been split into lines:", lines);
 
   // Your part-specific logic goes here
